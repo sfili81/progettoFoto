1f8f510c3ad5c47410ba40fe6d190577a663a983
sfili81 <sfiligoi@iosystems.it>
2026-06-14 11:15:17 +0200

    Aggiunge nome a header

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -7,8 +7,6 @@ use common\widgets\Alert;
 use frontend\assets\AppAsset;
 use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
 use sfili81\ErrorReportWidget\ErrorReportWidget;
 
 AppAsset::register($this);
@@ -33,40 +31,14 @@ AppAsset::register($this);
 
     <?php $this->beginBody() ?>
 
-    <header>
+    <header class="bg-dark text-white py-3 fixed-top">
         <?php
-        NavBar::begin([
-            'brandLabel' => Yii::$app->name,
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-            ],
-        ]);
-        $menuItems = [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-        ];
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        }
-
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-            'items' => $menuItems,
-        ]);
-        if (Yii::$app->user->isGuest) {
-            echo Html::tag('div', Html::a('Login', ['/site/login'], ['class' => ['btn btn-link login text-decoration-none']]), ['class' => ['d-flex']]);
-        } else {
-            echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout text-decoration-none']
-                )
-                . Html::endForm();
-        }
-        NavBar::end();
+        $rootGara = \common\models\Gara::find()->where(['lvl' => 0])->one();
+        $garaName = $rootGara ? Html::encode($rootGara->name) : Html::encode(Yii::$app->name);
         ?>
+        <div class="container text-center">
+            <h4 class="mb-0"><?= $garaName ?></h4>
+        </div>
     </header>
 
     <main role="main" class="flex-shrink-0">
@@ -75,9 +47,6 @@ AppAsset::register($this);
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
             ]) ?>
             <?= Alert::widget() ?>
-
-            <!-- include language swithcer -->
-            <?= $this->render('_languageSwitcher'); ?>
 
             <?= $content ?>
         </div>
